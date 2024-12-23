3bbdb6bed2d0b905f7d908d206ab84b9c91a0121
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-22 17:18:01 -0800

    Upgrade modules

---
+++ src/index.mjs
@@ -0,0 +1,18 @@
+import resolveOnce from 'resolve-once';
+
+export default function resolveOnceMap(fn) {
+  const resolvers = {};
+
+  return (key) => {
+    if (!resolvers[key]) {
+      resolvers[key] = resolveOnce(() => {
+        try {
+          return fn(key);
+        } catch (err) {
+          return Promise.reject(err);
+        }
+      });
+    }
+    return resolvers[key]();
+  };
+}
